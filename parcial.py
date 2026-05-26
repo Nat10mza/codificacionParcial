9cc23d76082eba0a8a9e53f4aad8ba9e5b4d7240
Nico Tramontina <ntramontina@civitas.com.ar>
2026-05-26 17:52:49 -0300

    feat: push & unshift if index > or < in codificacion and decodificacion

--- parcial.py
+++ parcial.py
@@ -9,7 +9,7 @@ def codificacion(value):
     consonantes = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "Ñ", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
     vocales= ["A","E","I","O","U"];
     desplazamiento_consonantes = 3
-    desplazamiento_vocal = 2
+    desplazamiento_vocales = 2
 
 
     distancia= len(palabra);
@@ -17,19 +17,32 @@ def codificacion(value):
 
 # recorrido de la palabra
     for i in range(distancia):
+        # Flag para agregar solo una vez las consonantes y vocales
+        push_consonantes = False
+        push_vocales = False
+
         # Chequear si letra es consonante
         
         for j in range(len(consonantes)):
+            # Pushear consonantes para evitar error de indice
+            if j + desplazamiento_consonantes >= len(consonantes) and not push_consonantes:
+                consonantes = consonantes + consonantes
+                push_consonantes = True
+
             if consonantes[j] == palabra[i]:  
                 palabra_codifica[i] = consonantes[j + desplazamiento_consonantes]
-                print(palabra_codifica[i]);
         
         for k in range(len(vocales)):
+            if k + desplazamiento_vocales >= len(vocales) and not push_vocales:
+                vocales = vocales + vocales
+                push_vocales = True
+
             if vocales[k] == palabra[i]:  
-                palabra_codifica[i] = vocales[k + desplazamiento_vocal]        
+                palabra_codifica[i] = vocales[k + desplazamiento_vocales]        
 
     respuesta = "".join(palabra_codifica);
     return respuesta;
+
 
 
 def decodificacion(value):
@@ -39,23 +52,31 @@ def decodificacion(value):
     consonantes = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "Ñ", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
     vocales= ["A","E","I","O","U"];
     desplazamiento_consonantes = 3
-    desplazamiento_vocal = 2
+    desplazamiento_vocales = 2
     distancia= len(palabra);
     palabra_codifica= np.empty(distancia, dtype='U10')
 
 # recorrido de la palabra
     for i in range(distancia):
+        # Flag para agregar solo una vez las consonantes y vocales
+        unshift_consonantes = False
+        unshift_vocales = False
+
         # Chequear si letra es consonante
         
         for j in range(len(consonantes)-1,-1,-1):
-            if consonantes[j] == palabra[i]:  
+            if j - desplazamiento_consonantes < 0 and not unshift_consonantes:
+                consonantes = consonantes + consonantes
+                unshift_consonantes = True
+            if consonantes[j] == palabra[i]: 
                 palabra_codifica[i] = consonantes[j - desplazamiento_consonantes]
-                print(palabra_codifica[i]);
         
         for k in range(len(vocales)-1,-1,-1):
+            if k - desplazamiento_vocales < 0 and not unshift_vocales:
+                vocales = vocales + vocales
+                unshift_vocales = True
             if vocales[k] == palabra[i]:  
-                palabra_codifica[i] = vocales[k - desplazamiento_vocal]
-                print(palabra_codifica[i]);
+                palabra_codifica[i] = vocales[k - desplazamiento_vocales]
         
 
     respuesta = "".join(palabra_codifica);
@@ -63,11 +84,8 @@ def decodificacion(value):
 
 
 
-
-
-
 palabra_codificada = codificacion(palabra)
-print('Palabra despues de primera ejecucion',palabra_codificada);
+print('Palabra codificada: ',palabra_codificada);
 
-palabra_codificada_2 = decodificacion(palabra_codificada)
-print('Palabra despues de primera ejecucion',palabra_codificada_2);
+palabra_decodificada = decodificacion(palabra_codificada)
+print('Palabra decodificada: ',palabra_decodificada);
